c81e1013ea50620862e0675b1d733c03234d6054
Drew Swinburne <drewswinburne@gmail.com>
2016-12-05 21:27:45 -0500

    adds create to showpages

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,13 +2,14 @@ class CarsController < ApplicationController
 
   def index
     @cars = Car.all
-    render :json => @cars.to_json(:include => :photos) 
+    render :json => @cars.to_json(:include => :photos)
   end
 
   def show
     @car = Car.find(params[:id])
-    render json: @car
-
+    render json: @car.to_json(:include => :photos)
   end
+
+
 
 end
